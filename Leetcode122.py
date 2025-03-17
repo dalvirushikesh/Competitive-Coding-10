4774fbeccdf674396f141ab80f63be54d75e9f4e
Rushikesh Dalvi <rushikeshdalvi22@gmail.com>
2025-03-17 19:09:44 -0400

    completed mock 10

---
+++ Leetcode122.py
@@ -0,0 +1,10 @@
+#Time Complexity = O(n)
+#Space Complexity = O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                maxProfit += (prices[i]-prices[i-1])
+        return maxProfit
